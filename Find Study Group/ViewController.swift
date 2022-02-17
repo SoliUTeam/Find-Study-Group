ddb41e0c7d4bcfa7971bd19fe0ea979f6e8c5ceb
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2022-02-16 20:52:27 -0600

    Initial Commit

---
+++ Find Study Group/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find Study Group
+//
+//  Created by JungpyoHong on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
