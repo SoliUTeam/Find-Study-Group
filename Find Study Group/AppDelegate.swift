563d49a0e41f2082f8197077b4711e2a02fa0240
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2022-03-27 15:57:19 -0500

    update appearance and rotation disabled

--- Find Study Group/AppDelegate.swift
+++ Find Study Group/AppDelegate.swift
@@ -11,8 +11,6 @@ import PLOP
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 #if DEBUG
         let component1 = SwitchPLOPComponent(title: FeatureFlag.homeScreen.rawValue,
@@ -42,5 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        let restrictRotation: UIInterfaceOrientationMask = .portrait
+        
+        return restrictRotation
     }
 }
